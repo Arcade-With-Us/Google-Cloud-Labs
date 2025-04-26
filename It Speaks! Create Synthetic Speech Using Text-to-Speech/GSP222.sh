d9bea9a2e7c43f2b3153ab10537905f02f14fe2c
Arcade With Us <abir.arcade.2@gmail.com>
2025-04-26 16:47:58 +0530

    Create GSP222.sh

---
+++ It Speaks! Create Synthetic Speech Using Text-to-Speech/GSP222.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define color variables
+YELLOW_COLOR=$'\033[0;33m'
+NO_COLOR=$'\033[0m'
+BACKGROUND_RED=`tput setab 1`
+GREEN_TEXT=`tput setab 2`
+RED_TEXT=`tput setaf 1`
+BOLD_TEXT=`tput bold`
+RESET_FORMAT=`tput sgr0`
+
+# Display initiation message
+echo "${BACKGROUND_RED}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+gcloud services enable texttospeech.googleapis.com
+
+gcloud iam service-accounts create tts-qwiklab
+
+gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwiklab@$PROJECT_ID.iam.gserviceaccount.com
+
+export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
+
+# Completion message
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
