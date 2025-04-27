b9241656deca58da1f256ce72fb131a0478a58f6
Arcade With Us <abir.arcade.2@gmail.com>
2025-04-27 16:52:47 +0530

    Create GSP072.sh

---
+++ BigQuery: Qwik Start - Console/GSP072.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Bright Foreground Colors
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Displaying start message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}                  Starting the process...                   ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
+echo
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT} ========================== Step 1: Querying Public Data ========================== ${RESET_FORMAT}"
+echo
+bq query --use_legacy_sql=false \
+"
+#standardSQL
+SELECT
+ weight_pounds, state, year, gestation_weeks
+FROM
+ \`bigquery-public-data.samples.natality\`
+ORDER BY weight_pounds DESC LIMIT 10;
+"
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT} ========================== Step 2: Create a Dataset ========================== ${RESET_FORMAT}"
+echo "${YELLOW_TEXT}${BOLD_TEXT} This step will create a new dataset called 'babynames'. ${RESET_FORMAT}"
+echo
+bq mk babynames
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
