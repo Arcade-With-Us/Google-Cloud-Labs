ea6d31ce91d81c41a1e9ec97c0c0024d2a0e0d8d
Arcade With Us <abir.arcade.2@gmail.com>
2025-04-27 15:50:49 +0530

    Update GSP421.sh

--- APIs Explorer: Cloud Storage/GSP421.sh
+++ APIs Explorer: Cloud Storage/GSP421.sh
@@ -30,10 +30,11 @@ echo
 # Instruction: Downloading Demo Images
 echo "${BLUE_TEXT}${BOLD_TEXT}Step 2: Downloading Demo Images...${RESET_FORMAT}"
 
-curl -LO raw.githubusercontent.com/ArcadeCrew/Google-Cloud-Labs/refs/heads/main/APIs%20Explorer%20Cloud%20Storage/demo-image1.png
-curl -LO raw.githubusercontent.com/ArcadeCrew/Google-Cloud-Labs/refs/heads/main/APIs%20Explorer%20Cloud%20Storage/demo-image2.png
-curl -LO raw.githubusercontent.com/ArcadeCrew/Google-Cloud-Labs/refs/heads/main/APIs%20Explorer%20Cloud%20Storage/demo-image1-copy.png
+curl -LO https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image1-copy.png
 
+curl -LO https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image2.png
+
+curl -LO https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/main/APIs%20Explorer%3A%20Cloud%20Storage/demo-image1-copy.png
 echo
 
 # Instruction: Uploading Images to Cloud Storage
