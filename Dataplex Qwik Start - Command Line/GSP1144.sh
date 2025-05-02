c1cd51ac70400b387d3fce1037c1feb0af5e1cff
Arcade With Us <abir.arcade.2@gmail.com>
2025-05-02 20:20:12 +0530

    Create GSP1144.sh

---
+++ Dataplex Qwik Start - Command Line/GSP1144.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Bright Foreground Colors
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+# Displaying start message
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}                  Starting the process...                   ${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
+echo
+
+# Instruction 1: Setting the Region
+echo
+echo "${YELLOW_TEXT}${BOLD_TEXT}========================== Setting the Region ========================== ${RESET_FORMAT}"
+echo "${YELLOW_TEXT}Enter REGION: ${RESET_FORMAT}"
+read -r REGION
+
+export REGION=$REGION
+
+echo "${YELLOW_TEXT}${BOLD_TEXT}Region set to: $REGION${RESET_FORMAT}"
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Enabling Dataplex API ========================== ${RESET_FORMAT}"
+
+gcloud services enable dataplex.googleapis.com
+
+export PROJECT_ID=$(gcloud config get-value project)
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}========================== Project ID fetched : ${PROJECT_ID} ========================== ${RESET_FORMAT}"
+
+gcloud config set compute/region $REGION
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Creating Lake ========================== ${RESET_FORMAT}"
+gcloud dataplex lakes create ecommerce \
+   --location=$REGION \
+   --display-name="Ecommerce" \
+   --description="Ecommerce Domain"
+echo "${GREEN_TEXT}${BOLD_TEXT}Lake ecommerce has been created successfully.${RESET_FORMAT}"
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Creating Zone ========================== ${RESET_FORMAT}"
+gcloud dataplex zones create orders-curated-zone \
+    --location=$REGION \
+    --lake=ecommerce \
+    --display-name="Orders Curated Zone" \
+    --resource-location-type=SINGLE_REGION \
+    --type=CURATED \
+    --discovery-enabled \
+    --discovery-schedule="0 * * * *"
+echo "${GREEN_TEXT}${BOLD_TEXT}Zone orders-curated-zone has been created successfully.${RESET_FORMAT}"
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Creating BigQuery Dataset ========================== ${RESET_FORMAT}"
+bq mk --location=$REGION --dataset orders 
+echo "${GREEN_TEXT}${BOLD_TEXT}BigQuery Dataset orders has been created successfully.${RESET_FORMAT}"
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Creating Asset ========================== ${RESET_FORMAT}"
+gcloud dataplex assets create orders-curated-dataset \
+--location=$REGION \
+--lake=ecommerce \
+--zone=orders-curated-zone \
+--display-name="Orders Curated Dataset" \
+--resource-type=BIGQUERY_DATASET \
+--resource-name=projects/$PROJECT_ID/datasets/orders \
+--discovery-enabled 
+echo "${GREEN_TEXT}${BOLD_TEXT}Asset orders-curated-dataset has been created successfully.${RESET_FORMAT}"
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Deleting Asset ========================== ${RESET_FORMAT}"
+gcloud dataplex assets delete orders-curated-dataset --location=$REGION --zone=orders-curated-zone --lake=ecommerce --quiet
+echo "${GREEN_TEXT}${BOLD_TEXT}Asset orders-curated-dataset has been deleted successfully.${RESET_FORMAT}"
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Deleting Zone ========================== ${RESET_FORMAT}"
+gcloud dataplex zones delete orders-curated-zone --location=$REGION --lake=ecommerce --quiet
+echo "${GREEN_TEXT}${BOLD_TEXT}Zone orders-curated-zone has been deleted successfully.${RESET_FORMAT}"
+
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}========================== Deleting Lake ========================== ${RESET_FORMAT}"
+gcloud dataplex lakes delete ecommerce --location=$REGION --quiet
+echo "${GREEN_TEXT}${BOLD_TEXT}Lake ecommerce has been deleted successfully.${RESET_FORMAT}"
+
+echo
+echo "${GREEN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}              Lab Completed Successfully!               ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
+echo
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
