c4fca876f0c7439462aeee640f2895045126887f
Arcade With Us <abir.arcade.2@gmail.com>
2025-05-09 11:21:44 +0530

    Update ARC130-1.sh

--- Analyze Sentiment with Natural Language API: Challenge Lab/ARC130-1.sh
+++ Analyze Sentiment with Natural Language API: Challenge Lab/ARC130-1.sh
@@ -1,28 +1,40 @@
-cat > analyze-request.json <<EOF_END
-{
-  "document":{
-    "type":"PLAIN_TEXT",
-    "content": "Google, headquartered in Mountain View, unveiled the new Android phone at the Consumer Electronic Show.  Sundar Pichai said in his keynote that users love their new Android phones."
-  },
-  "encodingType": "UTF8"
-}
-EOF_END
+#!/bin/bash
 
-curl -s -H "Content-Type: application/json" \
--H "Authorization: Bearer $(gcloud auth print-access-token)" \
-"https://language.googleapis.com/v1/documents:analyzeSyntax" \
--d @analyze-request.json > analyze-response.txt
+# Bright Foreground Colors
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
 
-cat > multi-nl-request.json <<EOF_END
-{
-  "document":{
-    "type":"PLAIN_TEXT",
-    "content":"Le bureau japonais de Google est situé à Roppongi Hills, Tokyo."
-  }
-}
-EOF_END
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
 
-curl -s -H "Content-Type: application/json" \
--H "Authorization: Bearer $(gcloud auth print-access-token)" \
-"https://language.googleapis.com/v1/documents:analyzeEntities" \
--d @multi-nl-request.json > multi-response.txt
+# Start of the script
+echo
+echo "${BLUE_TEXT}${BOLD_TEXT}Starting the process...${RESET_FORMAT}"
+echo
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Enter ZONE:${RESET_FORMAT}"
+read -r ZONE
+echo
+echo -e "${YELLOW_TEXT}${BOLD_TEXT}Enter API KEY:${RESET_FORMAT}"
+read -r API_KEY
+echo
+gcloud compute ssh lab-vm --zone=$ZONE --quiet --command "curl -LO https://raw.githubusercontent.com/ArcadeCrew/Google-Cloud-Labs/refs/heads/main/Analyze%20Sentiment%20with%20Natural%20Language%20API%20Challenge%20Lab/arcadecrew.sh && sudo chmod +x arcadecrew.sh && ./arcadecrew.sh"
+
+# # Safely delete the script if it exists
+# SCRIPT_NAME="arcadecrew.sh"
+# if [ -f "$SCRIPT_NAME" ]; then
+#     echo -e "${RED_TEXT}${BOLD_TEXT}Deleting the script ($SCRIPT_NAME) for safety purposes...${RESET_FORMAT}${NO_COLOR}"
+#     rm -- "$SCRIPT_NAME"
+# fi
+
+echo
+# Completion message
+echo -e "${GREEN_TEXT}${BOLD_TEXT}Lab Completed Successfully!${RESET_FORMAT}"
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
