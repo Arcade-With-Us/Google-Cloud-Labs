5aef6e1436f41130129f436989dea55736b4295d
Arcade With Us <abir.arcade.2@gmail.com>
2025-05-20 11:51:02 +0530

    Update ARC110.sh

--- Create a Streaming Data Lake on Cloud Storage Challenge Lab/ARC110.sh
+++ Create a Streaming Data Lake on Cloud Storage Challenge Lab/ARC110.sh
@@ -111,3 +111,6 @@ echo "${GREEN_TEXT}${BOLD_TEXT}=================================================
 echo "${GREEN_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
 echo "${GREEN_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
 echo
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
