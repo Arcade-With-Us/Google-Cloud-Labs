a685199f4c94e4a4e9b37b2f7050837d9a51216b
Arcade With Us <abir.arcade.2@gmail.com>
2025-06-10 20:26:48 +0530

    Update GSP777.sh

--- Autoscaling TensorFlow Model Deployments with TF Serving and Kubernetes/GSP777.sh
+++ Autoscaling TensorFlow Model Deployments with TF Serving and Kubernetes/GSP777.sh
@@ -75,3 +75,7 @@ kubectl autoscale deployment image-classifier \
 --max=4 
 
 kubectl get hpa
+
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
