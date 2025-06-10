742c794a08004fb5e0d2976e23a0a6228e44ab9e
Arcade With Us <abir.arcade.2@gmail.com>
2025-06-10 20:26:26 +0530

    Create GSP777.sh

---
+++ Autoscaling TensorFlow Model Deployments with TF Serving and Kubernetes/GSP777.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
+
+clear
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo "${GREEN_TEXT}${BOLD_TEXT}📍 Step 1: Retrieving default region & zone configuration...${RESET_FORMAT}"
+echo
+
+ZONE=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+PROJECT_ID=$(gcloud config get-value project)
+
+
+cd
+SRC_REPO=https://github.com/GoogleCloudPlatform/mlops-on-gcp
+kpt pkg get $SRC_REPO/workshops/mlep-qwiklabs/tfserving-gke-autoscaling tfserving-gke
+cd tfserving-gke
+
+
+gcloud config set compute/zone $ZONE
+
+PROJECT_ID=$(gcloud config get-value project)
+CLUSTER_NAME=cluster-1
+
+gcloud beta container clusters create $CLUSTER_NAME \
+  --cluster-version=latest \
+  --machine-type=e2-standard-4 \
+  --enable-autoscaling \
+  --min-nodes=1 \
+  --max-nodes=3 \
+  --num-nodes=1 
+
+export MODEL_BUCKET=${PROJECT_ID}-bucket
+gsutil mb gs://${MODEL_BUCKET}
+
+gsutil cp -r gs://spls/gsp777/resnet_101 gs://${MODEL_BUCKET}
+
+echo $MODEL_BUCKET
+sed -i "s/your-bucket-name/$MODEL_BUCKET/g" tf-serving/configmap.yaml
+
+kubectl apply -f tf-serving/configmap.yaml
+
+kubectl apply -f tf-serving/deployment.yaml
+
+kubectl get deployments
+
+kubectl apply -f tf-serving/service.yaml
+
+kubectl get svc image-classifier
+
+
+
+kubectl autoscale deployment image-classifier \
+--cpu-percent=60 \
+--min=1 \
+--max=4 
+
+kubectl get hpa
