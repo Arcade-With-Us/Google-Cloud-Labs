07cbeb732ac90e64d70bf8458d5aa995f01553b4
Arcade With Us <abir.arcade.2@gmail.com>
2025-06-10 21:06:01 +0530

    Update GSP182.sh

--- Distributed Load Testing Using Kubernetes/GSP182.sh
+++ Distributed Load Testing Using Kubernetes/GSP182.sh
@@ -70,4 +70,7 @@ kubectl get pods -l app=locust-worker
 
 echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
 
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
 #-----------------------------------------------------end---------------
