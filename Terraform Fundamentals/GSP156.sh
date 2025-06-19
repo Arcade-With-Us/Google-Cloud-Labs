eb095130e96f47e281debcbedc7d8e97a8b8c2ea
Arcade With Us <abir.arcade.2@gmail.com>
2025-06-19 11:15:17 +0530

    Update GSP156.sh

--- Terraform Fundamentals/GSP156.sh
+++ Terraform Fundamentals/GSP156.sh
@@ -19,3 +19,33 @@ echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}
 echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo
+
+gcloud auth list 
+
+cat > instance.tf <<EOF_END
+resource "google_compute_instance" "terraform" {
+  project      = "$DEVSHELL_PROJECT_ID"
+  name         = "terraform"
+  machine_type = "e2-medium"
+  zone         = "$ZONE"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+EOF_END
+
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
+
