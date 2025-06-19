60476dceddfb0db90822eddbd93c702b9cf013ef
Arcade With Us <abir.arcade.2@gmail.com>
2025-06-19 11:28:31 +0530

    Update GSP233.sh

--- Deploy Kubernetes Load Balancer Service with Terraform/GSP233.sh
+++ Deploy Kubernetes Load Balancer Service with Terraform/GSP233.sh
@@ -31,3 +31,14 @@ cd tf-gke-k8s-service-lb
 terraform init
 
 terraform apply -var="region=$REGION" -var="location=$ZONE" --auto-approve
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}" 
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
