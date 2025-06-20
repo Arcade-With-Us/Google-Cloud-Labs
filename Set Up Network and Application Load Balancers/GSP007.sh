ca6c3bbd65840cd52a9b44bab32afca5d705cefd
Arcade With Us <abir.arcade.2@gmail.com>
2025-06-20 14:06:12 +0530

    Update GSP007.sh

--- Set Up Network and Application Load Balancers/GSP007.sh
+++ Set Up Network and Application Load Balancers/GSP007.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+!/bin/bash
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
 
-# Define color variables
-YELLOW_COLOR=$'\033[0;33m'
-MAGENTA_COLOR="\e[35m"
-NO_COLOR=$'\033[0m'
-BACKGROUND_RED=`tput setab 1`
-GREEN_TEXT=$'\033[0;32m'
-RED_TEXT=`tput setaf 1`
-BOLD_TEXT=`tput bold`
-RESET_FORMAT=`tput sgr0`
-BLUE_TEXT=`tput setaf 4`
+clear
 
-echo ""
-echo ""
-
-# Display initiation message
-echo "${GREEN_TEXT}${BOLD_TEXT}Initiating Execution...${RESET_FORMAT}"
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
 
 echo
 
