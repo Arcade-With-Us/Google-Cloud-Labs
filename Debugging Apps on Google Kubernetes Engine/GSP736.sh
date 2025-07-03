40975293724b2eace2ce981cf0961987fa412d83
Arcade With Us <abir.arcade.2@gmail.com>
2025-07-03 12:42:21 +0530

    Update GSP736.sh

--- Debugging Apps on Google Kubernetes Engine/GSP736.sh
+++ Debugging Apps on Google Kubernetes Engine/GSP736.sh
@@ -136,7 +136,11 @@ echo "${BLUE_TEXT}${BOLD_TEXT}🛡️ Creating the Cloud Monitoring alert policy
 gcloud alpha monitoring policies create --policy-from-file="arcadewithus.json"
 echo
 
-echo "${BG_RED}${BOLD}Congratulations For Completing The Lab !!!${RESET}"
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
 echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
 #-----------------------------------------------------end----------------------------------------------------------#
