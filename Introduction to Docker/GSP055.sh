1d294ccf12280e980a5bb4b48dde45f731eb300c
Arcade With Us <abir.arcade.2@gmail.com>
2025-07-10 14:26:36 +0530

    Update GSP055.sh

--- Introduction to Docker/GSP055.sh
+++ Introduction to Docker/GSP055.sh
@@ -210,3 +210,15 @@ if [ -f "$SCRIPT_NAME" ]; then
     echo -e "${RED_TEXT}${BOLD_TEXT}Deleting the script ($SCRIPT_NAME) for safety purposes...${RESET_FORMAT}${NO_COLOR}"
     rm -- "$SCRIPT_NAME"
 fi
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo "" 
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}" 
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
