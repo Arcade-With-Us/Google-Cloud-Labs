cb4de7f6187e9dbdd26b7c340a5ef267e0750cef
Arcade With Us <abir.arcade.2@gmail.com>
2025-07-10 15:05:57 +0530

    Update GSP123.sh

--- Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/GSP123.sh
+++ Introduction to Cloud Dataproc Hadoop and Spark on Google Cloud/GSP123.sh
@@ -71,19 +71,19 @@ while [ "$cp_success" = false ]; do
   exit_status=$?
 
   if [ "$exit_status" -eq 0 ]; then
-    echo "Function deployed successfully [https://www.youtube.com/@techcps]"
+    echo "Function deployed successfully"
     cp_success=true
   else
     echo "Cluster creation failed. Checking if cluster already exists..."
 
     if gcloud dataproc clusters describe "$CLUSTER_NAME" --region "$REGION" &>/dev/null; then
-      echo "Cluster already exists. Deleting. [https://www.youtube.com/@techcps]"
+      echo "Cluster already exists. Deleting.."
       gcloud dataproc clusters delete "$CLUSTER_NAME" --region "$REGION" --quiet
       echo "Cluster deleted. Retrying in 10 seconds..."
     else
-      echo "Unknown errors. Retrying in 10 seconds[https://www.youtube.com/@techcps]"
+      echo "Unknown errors. Retrying in 10 seconds"
     fi
-    echo "Please subscribe to techcps [https://www.youtube.com/@techcps]"
+    echo "Please subscribe to techcps [https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l]"
     sleep 10
   fi
 done
