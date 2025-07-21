6aad4a09009dbdb633d527cd1206c20de03469d5
Arcade With Us <abir.arcade.2@gmail.com>
2025-07-21 11:37:10 +0530

    Update GSP345.sh

--- Build Infrastructure with Terraform on Google Cloud: Challenge Lab/GSP345.sh
+++ Build Infrastructure with Terraform on Google Cloud: Challenge Lab/GSP345.sh
@@ -7,16 +7,19 @@ BLUE_TEXT=$'\033[0;94m'
 MAGENTA_TEXT=$'\033[0;95m'
 CYAN_TEXT=$'\033[0;96m'
 WHITE_TEXT=$'\033[0;97m'
-RESET_FORMAT=$'\033[0m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
 BOLD_TEXT=$'\033[1m'
-UNDERLINE_TEXT=$'\033[4m'
+RESET_FORMAT=$'\033[0m'
+
 clear
 
 echo
-echo "${CYAN_TEXT}${BOLD_TEXT}==============================================${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}           INITIATING EXECUTION...          ${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}==============================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo
+
 
 echo -e "${YELLOW_TEXT}${BOLD_TEXT}Enter the BUCKET name: ${RESET_FORMAT}\c"
 read BUCKET
@@ -434,6 +437,14 @@ terraform plan
 
 terraform apply -auto-approve
 
-echo "" 
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
