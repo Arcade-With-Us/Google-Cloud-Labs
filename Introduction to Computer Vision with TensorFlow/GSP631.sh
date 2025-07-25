5e0aa6ec737639997f459898c6c9537ac4a956d6
Arcade With Us <abir.arcade.2@gmail.com>
2025-07-25 20:41:29 +0530

    Update GSP631.sh

--- Introduction to Computer Vision with TensorFlow/GSP631.sh
+++ Introduction to Computer Vision with TensorFlow/GSP631.sh
@@ -43,7 +43,7 @@ echo
 echo "${BLUE}${BOLD}Downloading lab notebooks...${RESET}"
 wget https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/blob/main/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/model%20(1).ipynb
 wget https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/blob/main/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/callback_model.ipynb
-wget https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/blob/main/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/model%20(1).ipynb
+wget https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/blob/main/Introduction%20to%20Computer%20Vision%20with%20TensorFlow/updated_model.ipynb
 echo
 
 echo
