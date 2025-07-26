e41ab9d4533b3d05dc69a6840eb796d3d56a13d4
Arcade With Us <abir.arcade.2@gmail.com>
2025-07-27 00:23:03 +0530

    Update GSP213.sh

--- VPC Networks - Controlling Access/GSP213.sh
+++ VPC Networks - Controlling Access/GSP213.sh
@@ -7,17 +7,18 @@ BLUE_TEXT=$'\033[0;94m'
 MAGENTA_TEXT=$'\033[0;95m'
 CYAN_TEXT=$'\033[0;96m'
 WHITE_TEXT=$'\033[0;97m'
-RESET_FORMAT=$'\033[0m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
 BOLD_TEXT=$'\033[1m'
-UNDERLINE_TEXT=$'\033[4m'
+RESET_FORMAT=$'\033[0m'
+
 clear
 
 echo
-echo "${CYAN_TEXT}${BOLD_TEXT}==============================================${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}             INITIATING EXECUTION          ${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}==============================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo
-
 
 gcloud auth list
 
