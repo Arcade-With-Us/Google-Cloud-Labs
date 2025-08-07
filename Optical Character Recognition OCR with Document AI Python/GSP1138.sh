f7aeb4f40e178bbe6867cb08b715c317ae9455f3
Arcade With Us <abir.arcade.2@gmail.com>
2025-08-07 11:43:09 +0530

    Update GSP1138.sh

--- Optical Character Recognition OCR with Document AI Python/GSP1138.sh
+++ Optical Character Recognition OCR with Document AI Python/GSP1138.sh
@@ -354,3 +354,15 @@ EOF_CP
 
 
 python3 batch_processing_directory.py
+
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
