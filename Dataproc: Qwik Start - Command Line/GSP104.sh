30c1fc88620e610d3ae2573e06fc341b0e8d5f97
Arcade With Us <abir.arcade.2@gmail.com>
2025-08-09 18:08:58 +0530

    Update GSP104.sh

--- Dataproc: Qwik Start - Command Line/GSP104.sh
+++ Dataproc: Qwik Start - Command Line/GSP104.sh
@@ -20,6 +20,25 @@ echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORM
 echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo
 
+gcloud config set dataproc/region $REGION
+
+PROJECT_ID=$(gcloud config get-value project) && \
+gcloud config set project $PROJECT_ID
+
+PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+  --member=serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+  --role=roles/storage.admin
+
+gcloud compute networks subnets update default --region=$REGION  --enable-private-ip-google-access
+
+gcloud dataproc clusters create example-cluster --worker-boot-disk-size 500 --worker-machine-type=e2-standard-4 --master-machine-type=e2-standard-4 --quiet
+
+gcloud dataproc jobs submit spark --cluster example-cluster \
+  --class org.apache.spark.examples.SparkPi \
+  --jars file:///usr/lib/spark/examples/jars/spark-examples.jar -- 1000
+
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
