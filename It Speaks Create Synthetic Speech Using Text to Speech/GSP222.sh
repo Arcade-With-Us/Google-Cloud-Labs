77e9ee06cb5589599f0162bd72b2ba98dc13c098
Arcade With Us <abir.arcade.2@gmail.com>
2025-08-20 22:18:33 +0530

    Update GSP222.sh

--- It Speaks Create Synthetic Speech Using Text to Speech/GSP222.sh
+++ It Speaks Create Synthetic Speech Using Text to Speech/GSP222.sh
@@ -30,9 +30,14 @@ gcloud iam service-accounts keys create tts-qwiklab.json --iam-account tts-qwikl
 
 export GOOGLE_APPLICATION_CREDENTIALS=tts-qwiklab.json
 # Completion message
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
 
-echo "" 
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
 #-----------------------------------------------------end----------------------------------------------------------#
