a035bf09c249b99379aa656a82363de18de1d167
Arcade With Us <abir.arcade.2@gmail.com>
2025-09-05 13:17:50 +0530

    Update GSP1089.sh

--- Cloud Functions 2nd Gen: Qwik Start/GSP1089.sh
+++ Cloud Functions 2nd Gen: Qwik Start/GSP1089.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+DIM_TEXT=$'\033[2m'
+STRIKETHROUGH_TEXT=$'\033[9m'
+BOLD_TEXT=$'\033[1m'
+RESET_FORMAT=$'\033[0m'
 
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-TEXT_RED=`tput setaf 1`
+clear
 
-BOLD=`tput bold`
-RESET=`tput sgr0`
-
-echo "${BG_RED}${BOLD}Starting Execution${RESET}"
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
 
 export REGION="${ZONE%-*}"
 gcloud config set compute/region $REGION
