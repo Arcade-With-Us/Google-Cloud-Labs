0f1cc506f554e32f80f92e9319e4888483dc8615
Arcade With Us <abir.arcade.2@gmail.com>
2025-09-05 13:18:31 +0530

    Update GSP1089.sh

--- Cloud Functions 2nd Gen: Qwik Start/GSP1089.sh
+++ Cloud Functions 2nd Gen: Qwik Start/GSP1089.sh
@@ -268,8 +268,14 @@ gcloud run deploy slow-function \
 --project=$DEVSHELL_PROJECT_ID
 
 echo -e "${TEXT_RED}${BOLD}Check The Score Upto Task 6 then Process Next${RESET}"
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
-
-echo "" 
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
