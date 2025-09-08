e81b0d3e9786ac6e4d2cc2ad0e9c6e5ff6fd091d
Arcade With Us <abir.arcade.2@gmail.com>
2025-09-08 12:53:28 +0530

    Update GSP1026.sh

--- Collect Metrics from Exporters using the Managed Service for Prometheus/GSP1026.sh
+++ Collect Metrics from Exporters using the Managed Service for Prometheus/GSP1026.sh
@@ -104,12 +104,14 @@ gsutil -m acl set -R -a public-read gs://$PROJECT
 echo "${GREEN}✅ Node exporter setup complete!${RESET}"
 echo
 
-# Completion Message
-echo "${BG_GREEN}${BOLD}╔════════════════════════════════════════════════════════╗${RESET}"
-echo "${BG_GREEN}${BOLD}          GMP MONITORING TUTORIAL COMPLETE!             ${RESET}"
-echo "${BG_GREEN}${BOLD}╚════════════════════════════════════════════════════════╝${RESET}"
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
-
-echo "" 
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
