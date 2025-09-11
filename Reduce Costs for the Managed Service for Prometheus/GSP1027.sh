9cd6bf516d117f2579974aff0f2de8c3eaa3203d
Arcade With Us <abir.arcade.2@gmail.com>
2025-09-11 11:46:07 +0530

    Update GSP1027.sh

--- Reduce Costs for the Managed Service for Prometheus/GSP1027.sh
+++ Reduce Costs for the Managed Service for Prometheus/GSP1027.sh
@@ -118,9 +118,14 @@ echo
 echo "${WHITE_TEXT}${BOLD_TEXT}🔐 Re-applying Public Read Access (if necessary) 🔐${RESET_FORMAT}"
 gsutil -m acl set -R -a public-read gs://$PROJECT
 
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
 
-echo "" 
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
 #-----------------------------------------------------end----------------------------------------------------------#
