062474383093938dea0e73a0747b2eaeb729437d
Arcade With Us <abir.arcade.2@gmail.com>
2025-09-18 21:39:29 +0530

    Update GSP321.sh

--- Develop your Google Cloud Network: Challenge Lab/GSP321.sh
+++ Develop your Google Cloud Network: Challenge Lab/GSP321.sh
@@ -199,8 +199,14 @@ for USER in $USERS; do
   fi
 done
 
-echo "${RED}${BOLD}Task 9. ${RESET}""${WHITE}${BOLD}Provide access for an additional engineer${RESET}" "${GREEN}${BOLD}Completed${RESET}"
-
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
