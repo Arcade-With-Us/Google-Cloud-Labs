104ff81c97a9c709865ea8f39df3dd4d150ec1a3
Arcade With Us <abir.arcade.2@gmail.com>
2025-10-05 20:13:19 +0530

    Update ArcadeWithUs.sh

--- mini lab : Cloud Storage : 2/ArcadeWithUs.sh
+++ mini lab : Cloud Storage : 2/ArcadeWithUs.sh
@@ -23,7 +23,7 @@ echo
 echo "Please set the below values correctly"
 read -p "Enter the BUCKET_NAME: " BUCKET_NAME
 
-curl -LO https://raw.githubusercontent.com/Arcade-With-Us/Google-Cloud-Labs/master/main/mini%20lab%20%3A%20Cloud%20Storage%20%3A%202/lifecycle.json
+curl -LO https://raw.githubusercontent.com/Techcps/GSP/master/mini%20lab%20%3A%20Cloud%20Storage%20%3A%202/lifecycle.json
 
 gsutil lifecycle set lifecycle.json gs://$BUCKET_NAME
 
