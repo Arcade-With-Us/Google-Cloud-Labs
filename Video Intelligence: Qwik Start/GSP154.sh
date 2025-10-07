623ca6bcc0f24c696cef36db790dc52b8982fb1a
Arcade With Us <abir.arcade.2@gmail.com>
2025-10-07 21:54:23 +0530

    Update GSP154.sh

--- Video Intelligence: Qwik Start/GSP154.sh
+++ Video Intelligence: Qwik Start/GSP154.sh
@@ -60,9 +60,14 @@ curl -s -H 'Content-Type: application/json' \
   -H "Authorization: Bearer $(gcloud auth print-access-token)" \
   "https://videointelligence.googleapis.com/v1/$OPERATION_NAME"
 
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
-
-#-----------------------------------------------------end----------------------------------------------------------#
-echo "" 
-echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
 echo
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
+echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
+echo
+
+echo ""
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe to my Channel (Arcade With Us):${RESET_FORMAT}"
+echo -e "${BLUE_TEXT}${BOLD_TEXT}https://youtube.com/@arcadewithus_we?si=yeEby5M3k40gdX4l${RESET_FORMAT}"
+echo
+#-----------------------------------------------------end----------------------------------------------------------#
