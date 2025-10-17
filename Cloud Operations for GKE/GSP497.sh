9c41d8ec204ff75e54e260b0da226690db816b40
Arcade With Us <abir.arcade.2@gmail.com>
2025-10-17 21:26:18 +0530

    Update GSP497.sh

--- Cloud Operations for GKE/GSP497.sh
+++ Cloud Operations for GKE/GSP497.sh
@@ -20,6 +20,41 @@ echo "${CYAN_TEXT}${BOLD_TEXT}🚀     INITIATING EXECUTION     🚀${RESET_FORM
 echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo
 
+# Set text styles
+YELLOW=$(tput setaf 3)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+echo "Please set the below values correctly"
+read -p "${YELLOW}${BOLD}Enter the ZONE: ${RESET}" ZONE
+
+# Export variables after collecting input
+export ZONE
+
+
+gcloud auth list
+
+export PROJECT_ID=$(gcloud config get-value project)
+
+export REGION=${ZONE%-*}
+
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+
+
+gsutil cp gs://spls/gsp497/gke-monitoring-tutorial.zip .
+unzip gke-monitoring-tutorial.zip
+
+cd gke-monitoring-tutorial
+
+make create
+
+
+make validate
+
+make teardown
+
+
 echo
 echo "${CYAN_TEXT}${BOLD_TEXT}===================================${RESET_FORMAT}"
 echo "${CYAN_TEXT}${BOLD_TEXT}🚀  LAB COMPLETED SUCCESSFULLY  🚀${RESET_FORMAT}"
